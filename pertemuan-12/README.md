3cd697273bb8237a74d0bc5fd9df605b1a5a074a
Arief Budikurniawan <2511500081@mahasiswa.atmaluhur.ac.id>
2025-12-18 17:11:44 +0700

    tambah link edit, cid di-casting saat output (HTML)

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -1,1 +1,13 @@
-# pertemuan-12
+# pertemuan-10
+
+<ol>
+  <li>copy-paste file yang ada di pertemuan-11 ke pertemuan-12</li>
+  <li>tambah kolom aksi</li>
+  <li>tambah link edit, cid di-casting saat output (HTML)</li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li> 
+</ol>  
---
+++ pertemuan-12/read.php
@@ -0,0 +1,34 @@
+<?php
+require 'koneksi.php';
+
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
+$no = 1;
+
+?>
+
+<table border="1" cellpadding="8" cellspacing="0">
+
+    <tr>
+        <th>No</th>
+        <th>Aksi</th>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Pesan</th>
+        <th>Created At</th>
+    </tr>
+
+    <?php while ($row = mysqli_fetch_assoc($q)): ?>
+        <tr>
+            <td><?= $no++; ?></td>
+            <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
+            <td><?= $row['cid']; ?></td>
+            <td><?= $row['cnama']; ?></td>
+            <td><?= $row['cemail']; ?></td>
+            <td><?= $row['cpesan']; ?></td>
+            <td><?= $row['created_at']; ?></td>
+        </tr>
+
+    <?php endwhile; ?>
+</table>
